3b5aeef02aef697d2869488571baa3c298846224
Your Name <ajisurya331@gmail.com>
2025-07-14 17:21:45 +0700

    add list skill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "*.{html,js}",
+    "*.html",
+    "./src/input.css",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     fontFamily:{
       'poppins': 'Poppins'
+    },
+   extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' }, // scroll separuh lebar
+        }
+      },
+      animation: {
+        scroll: 'scroll 20s linear infinite', // ganti durasi sesuai kebutuhan
+      }
     },
      container: {
       center: true,
